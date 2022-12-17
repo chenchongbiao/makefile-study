b4c62ea6a60c6c73f39c8c88e64ec0e0df04852a
bluesky <379865549@qq.com>
2022-12-17 18:49:35 +0800

    添加cmake install 导出例子

---
+++ CMake/code/cmake-install-export/test_hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "test_hello.h"
+
+int test_hello(void)
+{
+	printf("Hello World\n");
+	return 0;
+}
---
+++ CMake/code/cmake-install-export/test_hello.h
@@ -0,0 +1,6 @@
+#ifndef __TEST_HELLO_H__
+#define __TEST_HELLO_H__
+
+int test_hello(void);
+
+#endif
