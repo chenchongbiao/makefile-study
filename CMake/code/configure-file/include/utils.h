beef78c0675b7fe6ea37eede1a652509c6ef2299
bluesky <379865549@qq.com>
2022-12-18 11:17:07 +0800

    添加configure_file的例子

---
+++ CMake/code/configure-file/include/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H_IN
+#define UTILS_H_IN
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+
+#define BUILD_TIMESTAMP "2022-12-18 11:08:20"
+
+#define GIT_BRANCH "master"
+#define GIT_HASH "6f2011a"
+
+#endif // UTILS_H_IN
